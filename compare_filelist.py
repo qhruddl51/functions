251a5afd379ccd223507c37de07ab05f7f07bb88
titan21 <titan21@gridone.co.kr>
2021-10-25 16:08:48 +0900

    col_extract함수를 general하게 수정

--- compare_filelist.py
+++ compare_filelist.py
@@ -33,11 +33,18 @@ def filenames_csv (root_dir, folder) :
 
 
 
-# csv에서 특정 컬럼에 있는 파일이름들을 뽑아 리스트로 반환
-def csv_col_extract(col:int, csvfile:str) : 
+# txt, csv에서 특정 컬럼에 있는 파일이름들을 뽑아 리스트로 반환
+# separtor에 구분기호 전달 
+def col_extract(col:int, file:str, separtor=",") : 
     with open(csvfile, "r") as f :
         lines = f.readlines()
-        file_list = [re.split(",", line)[col-1] for line in lines]
+        lines = [re.split(separtor, line) for line in lines]
+        file_list = [line[col-1] for line in lines]
+
+        if (len(lines[0]) == col) & (not file_list[-1].endswith("\n")) :
+            file_list[-1] = file_list[-1]+"\n"
+
+        file_list = [line for line in file_list]
     return file_list
     
     
@@ -85,9 +92,9 @@ if __name__ == "__main__" :
     
     #==================================================#
     if input_mode == "csv" :
-        filelist_1 = csv_col_extract(col=1, csvfile=f"{root_dir}/{category_names[0]}.csv")
-        filelist_2 = csv_col_extract(col=1, csvfile=f"{root_dir}/{category_names[1]}.csv")
-        total_filelist = csv_col_extract(col=1, csvfile=f"{root_dir}/{category_names[2]}.csv")
+        filelist_1 = col_extract(col=1, csvfile=f"{root_dir}/{category_names[0]}.csv")
+        filelist_2 = col_extract(col=1, csvfile=f"{root_dir}/{category_names[1]}.csv")
+        total_filelist = col_extract(col=1, csvfile=f"{root_dir}/{category_names[2]}.csv")
         
         print("category1 : ", f"{root_dir}/{category_names[0]}.csv :: {len(filelist_1)}")
         print("category2 : ", f"{root_dir}/{category_names[1]}.csv :: {len(filelist_2)}")
