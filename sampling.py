b3aa5c0147fcfea8001a5c8b6060fde3249ddc61
titan21 <titan21@gridone.co.kr>
2021-12-20 10:14:11 +0900

    인덱스 아규먼트 추가

--- sampling.py
+++ sampling.py
@@ -7,12 +7,17 @@ import random
 
 # origin_folder_path 폴더에서 이미지 파일을 랜덤하게 percent만큼 뽑아서 
 # new_folder_path 폴더로 복사하는 함수 
-# idxs를 반환
-def file_copy_move(percent, origin_folder_path, new_folder_path) :
+# 복사한 파일의 인덱스들을 반환
+def sampling_in_folder(percent, origin_folder_path, new_folder_path, idxs=None) :
     filenames = os.listdir(f"{origin_folder_path}")
     filenames.sort()
-    n = int(len(filenames) * percent / 100)
-    idxs = random.sample(population=range(0, len(filenames)), k=n)
+    
+    if idxs : # 인덱스 리스트를 주면,
+        pass
+    else : # 인덱스 리스트를 안주면, 인덱스 리스트를 새로 만든다. 
+        n = int(len(filenames) * percent / 100)
+        # random.sample() : 리스트에서 여러 가지를 랜덤으로 추출하기(중복 허용 X)
+        idxs = random.sample(population=range(0, len(filenames)), k=n)
     
     for idx in idxs :
         shutil.copy2(f"{origin_folder_path}/{filenames[idx]}", f"{new_folder_path}/{filenames[idx]}")
