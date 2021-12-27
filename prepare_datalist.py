2769483ff87302ad0fb75d4c888014f773fdd124
titan21 <titan21@gridone.co.kr>
2021-12-27 11:00:54 +0900

    데이터-레이블 리스트를 csv로 만들어주는 함수 추가

---
+++ prepare_datalist.py
@@ -0,0 +1,19 @@
+import os
+import re
+
+# root_dir : 이미지와 레이블 디렉토리가 있는 루트 디렉토리
+# 이미지 디렉토리 이름
+# 레이블 디렉토리 이름
+# 루트 디렉토리에 labels.csv 파일을 생성해준다. 
+def prepare_datalist (root_dir:str , images_dir:str, labels_dir:str) : 
+    images = os.listdir(f"{root_dir}/{images_dir}")
+    labels = os.listdir(f"{root_dir}/{labels_dir}")
+    
+    with open(f"{root_dir}/labels.csv", "w", encoding='utf8') as f : 
+        for i, l in zip(images, labels) :
+            f.write(f"{root_dir}/{images_dir}s/{i} \t {root_dir}/{labels_dir}/{l} \n")
+    pass # prepare_datalist
+
+
+if __name__ == "__main__" :
+    prepare_datalist("C:\source\my_workspace\my_data\obj", "images", "labels")
